a4289f99e3c4bb2c8e79036d028e72352c00ae9f
mingoojo <swyks112@gmail.com>
2025-09-16 16:02:23 +0900

    metadata수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: "Mingoo Jo's Blog",
   author: 'Mingoo Jo',
   headerTitle: 'MJ Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'React, Spring, 그리고 개발 경험을 기록하는 개인 블로그',
   language: 'ko',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -22,7 +22,7 @@ const siteMetadata = {
   instagram: 'https://www.instagram.com',
   medium: 'https://medium.com',
   bluesky: 'https://bsky.app/',
-  locale: 'en-US',
+  locale: 'ko-KR',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
