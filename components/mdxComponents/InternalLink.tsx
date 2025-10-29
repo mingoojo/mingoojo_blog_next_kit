3f7f8e402c035963e4a187760eec427d7962994e
mingoojo <swyks112@gmail.com>
2025-10-29 09:42:08 +0900

    1. terms link 수정

--- components/mdxComponents/InternalLink.tsx
+++ components/mdxComponents/InternalLink.tsx
@@ -4,37 +4,39 @@ import React from 'react'
 type Props = {
   link: string
   name: string
+  file: string
 }
 
 
 export default function InternalLink({
   link,
   name,
+  file
 }: Props) {
 
 
   const baseUrl = "https://mingoojo-blog-next-kit.vercel.app/"
 
-  const urls = {
-    author: "Jo Mingu",
-    github: "https://github.com/jomingu",
-    project: "KPOP-MultiModule",
-    schema: `${baseUrl}/blog/sqld/[%EC%9E%90%EA%B2%A9%EC%A6%9D]SQLD%20%EA%B4%80%EB%A0%A8%20%EC%9A%A9%EC%96%B4%EC%84%A4%EB%AA%85#%EC%8A%A4%ED%82%A4%EB%A7%88schema`,
-    transaction: `${baseUrl}/blog/sqld/[자격증]SQLD%20관련%20용어설명#트랜젝션transaction`
-  }
+  const files = [{
+    name: "sqld", url: "blog/용어설명/SQLD%20관련%20용어설명"
+  }]
 
 
-  const href = urls[link as keyof typeof urls]
-
+  const fileUrl = files.filter((data) => data.name === file)[0].url
 
   // 혹시 해당 key가 없을 경우 대비
-  if (!href) {
+  if (!fileUrl || fileUrl.length !== 1) {
     console.warn(`Invalid link key: ${link}`)
     return <span>Invalid links</span>
   }
 
+  const hre2 = `${baseUrl}${files.filter((data) => data.name === file)[0].url}#${link}`
+  console.log(hre2)
+
+
+
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a href={hre2} target="_blank" rel="noopener noreferrer">
       {name}
     </a>
   )
